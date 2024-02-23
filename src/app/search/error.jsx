fc64b7e6e9520e3ae0411e004c200bb535a929d2
SUNIRMAL DAS <sunirmald1996@gmail.com>
2024-02-23 18:32:33 +0530

    handle error requests

---
+++ src/app/search/error.jsx
@@ -0,0 +1,21 @@
+'use client';
+import { useEffect } from 'react';
+import { TbReload } from "react-icons/tb";
+
+const  Error = ({ error, reset }) => {
+
+  useEffect(() => {
+    console.log('error', error);
+  }, [error]);
+
+  return (
+    <div className='flex flex-col justify-center items-center pt-10'>
+      <h1 className='text-3xl font-semibol text-slate-400 mb-3'>Something went wrong!</h1>
+      <button className=' flex flex-row gap-1 items-center font-semibold text-slate-400 text-xl'>
+      <TbReload className="font-semibold" />
+        Try again</button>
+    </div>
+  );
+}
+
+export default Error
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Link from 'next/link';
 
 const WebSearchPage = async ({searchParams}) => {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`)
+
+  if (!response.ok) throw new Error('Something went wrong');
+
   const data = await response.json();
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className='flex flex-col text-slate-500 justify-center items-center pt-14'>
+        <h1 className='text-3xl mb-4'>
+          No results found for {" "}
+          <span className="italic font-semibold">
+            {searchParams.searchTerm}
+          </span>
+        </h1>
+        <p className='text-md'>
+          Try searching the web or images for something, or else go{" "}
+          <Link href='/' className='hover:text-blue-500 underline'>
+            back
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
       {results && results.map((result, i) => <h1 key={i}>{result.title}</h1>)}
