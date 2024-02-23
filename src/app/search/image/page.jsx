37c35318691cb5e81805552cd91ef6e397686d58
SUNIRMAL DAS <sunirmald1996@gmail.com>
2024-02-23 21:10:56 +0530

    add image result component

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import ImageSearchResults from '@/components/ImageSearchResults';
+import Link from 'next/link';
 
-const ImageSearchPage = () => {
-  return (
-    <div>ImageSearchPage</div>
-  )
+const ImageSearchPage = async ({ searchParams }) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image`
+  );
+  if (!response.ok) throw new Error('Something went wrong');
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className='flex flex-col text-slate-500 justify-center items-center pt-14'>
+      <h1 className='text-3xl mb-4'>
+        No results found for {" "}
+        <span className="italic font-semibold">
+          {searchParams.searchTerm}
+        </span>
+      </h1>
+      <p className='text-md'>
+        Try searching the web or images for something, or else go{" "}
+        <Link href='/' className='hover:text-blue-500 underline'>
+          back
+        </Link>
+      </p>
+    </div>
+    );
+  }
+
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
 
 export default ImageSearchPage
--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { AiOutlineCamera, AiOutlineSearch } from 'react-icons/ai';
 import { GoChevronLeft } from "react-icons/go";
